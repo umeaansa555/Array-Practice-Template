e2ee9df8992f7b55192f28278cd01e1f7d236a18
Tony Theodoropoulos <theodot@outlook.com>
2021-09-29 10:04:55 -0400

    updated template with sample code

--- ArrayPractice/Form1.cs
+++ ArrayPractice/Form1.cs
@@ -21,20 +21,25 @@ namespace ArrayPractice
         private void runButton_Click(object sender, EventArgs e)
         {
             // Create 2 arrays to hold 10 values each
-
-
-            // variables that can be used to determine outcomes of questions
-            int samePosition;
-            int max;
-            double average;
-            double sum;
-
             Random randGen = new Random();
 
-            //1. Generate random values for both pre-created arrays
+            int[] array1 = new int[10];
+
+
+            //1. Generate random values for BOTH pre-created arrays
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = randGen.Next(0, 10);
+            }
 
 
             //2. Display array 1
+            array1Output.Text = "";
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1Output.Text += array1[i] + " ";
+            }
+
 
 
             //3. Display array 2
@@ -44,9 +49,12 @@ namespace ArrayPractice
 
 
             //5. Determine and display the largest value in array 1          
+            int max = 0;
 
 
             //6. Determine and display the average of all values in array 1
+            double sum = 0;
+            double average = 0;
 
 
             //7. Determine and display the sum of all even values in array 1
@@ -56,6 +64,8 @@ namespace ArrayPractice
 
 
             //9. Determine and display how many matching numbers in matching positions between the 2 arrays there are
+
+            int samePosition = 0;
 
 
             // ----------------------
